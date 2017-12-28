61c755c2e93bfda96c61f3c7da943b7ad3cf1eb7
Lidian Vilazio <lidianvilazio@hotmail.com>
2017-12-28 19:39:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,14 @@
 function forLoop(array) {
-  for(let i = 0; i <= 25; i++) {
+  for(var i = 0; i < array.length;i++) {
     if(i === 1) {
       array[i] = "I am 1 strange loop."
     } else {
       array[i] = `I am ${i} strange loops.`
+    }
+  }
+  if(array.length !== array.length+25) {
+    for(var l = array.length; l < array.length+25;l++) {
+      array[l] = `I am ${i} strange loops.`
     }
   }
 
